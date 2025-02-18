319653aa35b9177b860f7b7bd06bd907891d0cee
dallas-vanwyk <dallas.vanwyk@gmail.com>
2025-02-17 20:30:40 -0600

    building the show page; following tutorial

--- controllers/applications.js
+++ controllers/applications.js
@@ -11,16 +11,71 @@ const router = express.Router();
 const User = require('../models/user.js');
 
 // router logic goes here
-router.get('/', (req, res) => {
-    // res.send('hello job applications index route');
+// --------------------------------------------- Index
+router.get('/', async (req, res) => {
+    
     try {
-        res.render('applications/index.ejs')
+        
+        // look up user from req.session
+        const currentUser = await User.findById(req.session.user._id);
+
+        // pass in the current user's applications in the context object
+        res.render('applications/index.ejs', {
+            applications: currentUser.applications,
+        });
+    
     } catch (error) {
         console.log(error);
         res.redirect('/');
     };
 });
 
+// --------------------------------------------- Show
+router.get('/:applicationId', async (req, res) => {
+    
+    try {
+        const currentUser = await User.findById(req.session.user._id);
+        const application = currentUser.applications.id(req.params.applicationId);
+        res.render('applications/show.ejs', {
+            application: application,
+        });
+    } catch (error) {
+        console.log(error);
+        res.redirect('/');
+    };
+
+});
+
+// route for new job app form
+// --------------------------------------------- New
+router.get('/new', (req, res) => {
+    res.render('applications/new.ejs');
+});
+
+// route for submitting new job app
+// --------------------------------------------- Create
+router.post('/', async (req, res) => {
+
+    try {
+
+        // look up user for req.session
+        const currentUser = await User.findById(req.session.user._id);
+
+        // req.body is the new form data object
+        // push req.body to the applications array of the current user
+        currentUser.applications.push(req.body);
+
+        // save changes to the user
+        await currentUser.save();
+
+        // redirect back to applications index view
+        res.redirect(`/users/${currentUser._id}/applications`);
+    } catch (error) {
+        console.log(error);
+        res.redirect('/');
+    };
+
+});
 
 // export router for main server file
 module.exports = router;
